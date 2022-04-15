3221e409a216de9a6deac14031d6d5d10455c642
Sergey <svbashkirov@gmail.com>
2022-04-16 01:12:26 +0300

    Task 3 finished

---
+++ TASK_3/Program.cs
@@ -0,0 +1,8 @@
+﻿
+double number;
+Console.WriteLine("Введите число");
+while (!double.TryParse(Console.ReadLine(), out number))
+    Console.WriteLine("Введите ЧИСЛО");
+
+if (number % 2 == 0) Console.WriteLine("да");
+else Console.WriteLine("нет");
