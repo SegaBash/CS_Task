79bc2fe8a8dc3fb6bb81e833392c894d3dd3b73a
Bashkirov <svbashkirov@gmail.com>
2022-04-15 17:48:02 +0300

    Finished Task 1

--- TASK_1/Program.cs
+++ TASK_1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿double num1, num2;
+Console.WriteLine("Введите первое число");
+while (!double.TryParse(Console.ReadLine(), out num1))
+    Console.WriteLine("Введите первое ЧИСЛО");
+Console.WriteLine("Введите второе число");
+while (!double.TryParse(Console.ReadLine(), out num2))
+    Console.WriteLine("Введите второе ЧИСЛО");
+
+
+if (num1 == num2)
+    Console.WriteLine("max = min = " + num1);
+else
+{
+    if (num1 > num2)
+        Console.WriteLine("max = " + num1);
+    else
+        Console.WriteLine("max = " + num2);
+}
