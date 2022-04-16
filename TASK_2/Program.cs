8fc6295b31bf96bbcd72f45ca8fde9ba1154a02e
Sergey <svbashkirov@gmail.com>
2022-04-16 15:20:49 +0300

    Task 2 modified

--- TASK_2/Program.cs
+++ TASK_2/Program.cs
@@ -9,17 +9,7 @@ Console.WriteLine("Введите третье число");
 while (!double.TryParse(Console.ReadLine(), out num3))
     Console.WriteLine("Введите третье ЧИСЛО");
 
-if (num1 > num2)
-{
-    if (num1 > num3)
-        Console.WriteLine(num1);
-    else
-        Console.WriteLine(num3);
-}
-else
-{
-    if (num2 > num3)
-        Console.WriteLine(num2);
-    else
-        Console.WriteLine(num3);
-}
+double max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine(max);
