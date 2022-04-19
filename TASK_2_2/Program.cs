bddf343a4926f601452605ccfe280aaf45af8d23
Sergey <svbashkirov@gmail.com>
2022-04-19 22:56:25 +0300

    Task 2_3 finished

--- TASK_2_2/Program.cs
+++ TASK_2_2/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Введите целое трехзначное число");
-while (!int.TryParse(Console.ReadLine(), out int number) || number < 100 || number > 999)
+﻿int number;
+Console.WriteLine("Введите целое трехзначное число");
+while (!int.TryParse(Console.ReadLine(), out number) || number < 100 || number > 999)
     Console.WriteLine("Неверный ввод. Введите целое трехзначное число.");
 
-
-
+Console.WriteLine(number % 100 / 10);
---
+++ TASK_2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// примем в условии задачи что первое введенное число должно быть кратно второму. чтобы было соответствие премеру выполнения программы.
+int num1, num2;
+Console.WriteLine("Введите первое целое число");
+while (!int.TryParse(Console.ReadLine(), out num1))
+    Console.WriteLine("Неверный ввод. Введите первое целое число");
+Console.WriteLine("Введите второе целое число");
+while (!int.TryParse(Console.ReadLine(), out num2))
+    Console.WriteLine("Неверный ввод. Введите второе целое число");
+
+if (FindReminder(num1, num2) == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, остаток - {FindReminder(num1, num2)}");
+
+
+int FindReminder(int num1, int num2)
+{
+    return num1 % num2;
+}
+//Глупый конечно метод получился, но для практики и такой сойдет.
