6fed5f3bdff645778a38417249dd30cb0f9da617
Sergey <svbashkirov@gmail.com>
2022-04-19 23:47:19 +0300

    Task 2_4 modified

--- TASK_2_4/Program.cs
+++ TASK_2_4/Program.cs
@@ -1,7 +1,7 @@
 ﻿int number;
 Console.WriteLine("Введите целое число");
-while(!int.TryParse(Console.ReadLine(),out number))
-Console.WriteLine("Неверный ввод. Введите целое число");
+while (!int.TryParse(Console.ReadLine(), out number))
+    Console.WriteLine("Неверный ввод. Введите целое число");
 
-if(number%7==0&&number%23==0) Console.WriteLine("ДА");
+if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("ДА");
 else Console.WriteLine("НЕТ");
