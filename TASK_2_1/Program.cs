275a8b1855ca09f67cdae7cc3ab39d34fd871d1e
Sergey <svbashkirov@gmail.com>
2022-04-19 19:02:53 +0300

    Task 2.1 finished

---
+++ TASK_2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+Console.WriteLine(MaxDigitOfNumber(number));
+
+int MaxDigitOfNumber(int num)
+{
+    int digit1 = num / 10;
+    int digit2 = num % 10;
+    return digit1 > digit2 ? digit1 : digit2;
+}
