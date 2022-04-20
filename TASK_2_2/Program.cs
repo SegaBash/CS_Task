7afe2ab59650ce9b1ebd6aa50032e6acc46fcf18
Sergey <svbashkirov@gmail.com>
2022-04-20 23:40:46 +0300

    Task 2_7 finished

--- TASK_2_2/Program.cs
+++ TASK_2_2/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine("Введите целое трехзначное число")
 while (!int.TryParse(Console.ReadLine(), out number) || number < 100 || number > 999)
     Console.WriteLine("Неверный ввод. Введите целое трехзначное число.");
 
-Console.WriteLine(number % 100 / 10);
+Console.WriteLine(number / 10 % 10);
--- TASK_2_7/Program.cs
+++ TASK_2_7/Program.cs
@@ -1,7 +1,17 @@
-﻿// Предположим, что заказчик хочет введения значений из области рациональных чисел
+﻿// Предположим, что заказчик хочет введения значений из области рациональных чисел. Для интереса.
 double number;
 Console.WriteLine("Введите число");
 while (!double.TryParse(Console.ReadLine(), out number))
     Console.WriteLine("Неверный ввод. Введите число, пожалуйста");
-int digit3 = (int)(Math.Abs(number)/100);
-Console.WriteLine(digit3);
+
+double absNum = Math.Abs(number);
+//if (Math.Abs(number)>=100) Console.WriteLine(number.ToString().Replace("-","")[2]);
+if (absNum >= 100) Console.WriteLine(Find3Digit(absNum));
+else Console.WriteLine("третьей цифры нет");
+
+
+int Find3Digit(double num)
+{
+    int digit3 = (int)Math.Log10(num) + 1;
+    return (int)(num / Math.Pow(10, digit3 - 3) % 10);
+}
