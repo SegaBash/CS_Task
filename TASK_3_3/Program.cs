2b0af7bc3656ced1f62c4165207d44862a4d752c
Sergey <svbashkirov@gmail.com>
2022-04-23 01:57:12 +0300

    Task 3_3 finished

---
+++ TASK_3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int N;
+Console.WriteLine("Введите целое число > 1");
+while (!int.TryParse(Console.ReadLine(), out N) || N <= 1)
+    Console.WriteLine("Неверный ввод. Введите целое число > 1");
+
+for (int i = 1; i <= N; i++)
+{
+    if (i!=1) Console.Write(", ");
+    Console.Write(i * i);
+}
