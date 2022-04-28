a9ebfa3feef615871dbf17cebf2191bfe0e69c8e
Sergey <svbashkirov@gmail.com>
2022-04-29 00:39:27 +0300

    Task 4_4 finished

---
+++ TASK_4_4/Program.cs
@@ -0,0 +1,22 @@
+﻿var arr = new int[8];
+FillArray(arr);
+//Console.WriteLine("[" + String.Join(", ", arr) + "]");
+Console.WriteLine(PutArrayToString(arr));
+
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new Random().Next(1, 10);
+}
+
+string PutArrayToString(int[] arr)
+{
+    string str = "";
+    foreach (var val in arr)
+    {
+        if (str != "") str += ", ";
+        str += val;
+    }
+    return "[" + str + "]";
+}
