770e8893ca16e8ee604879fd0dbb5526dd6bb47c
Sergey <svbashkirov@gmail.com>
2022-04-28 23:16:24 +0300

    Task 4_3 finished

---
+++ TASK_4_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//принимаем область вводимых значений - множество целых чисел
+int number;
+Console.WriteLine("Ведите целое число");
+while (!int.TryParse(Console.ReadLine(), out number))
+    Console.WriteLine("Неверный ввод. Введите целое число");
+
+number = Math.Abs(number);
+Console.WriteLine(GetDigitSum(number));
+
+
+int GetDigitSum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
