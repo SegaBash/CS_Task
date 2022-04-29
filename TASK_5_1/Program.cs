a3c014cbdee28522f3654908593e0b4c1afe58b3
Sergey <svbashkirov@gmail.com>
2022-04-29 16:30:40 +0300

    Task 5_1 finished

---
+++ TASK_5_1/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = 5;
+var arr = new int[5];
+
+FillRandomArray(arr, 100, 999);
+Console.Write("Array of " + n + " elements: ");
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+Console.Write("Number of even elements: ");
+Console.WriteLine(GetEvenConunt(arr));
+
+
+
+void FillRandomArray(int[] arr, int lBound, int rBound)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(lBound, rBound + 1);
+    }
+}
+
+int GetEvenConunt(int[] arr)
+{
+    int even = 0;
+    foreach (var val in arr)
+        if (val % 2 == 0) even += 1;
+    return even;
+}
