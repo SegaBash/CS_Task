1d3d0832f52bd7d4354fca8b9d471345050a29cf
Sergey <svbashkirov@gmail.com>
2022-05-24 01:24:01 +0300

    Lesson 7 started

---
+++ LESSON_7/TASK_7_1/Program.cs
@@ -0,0 +1,52 @@
+﻿int rows = 4;
+int columns = 4;
+int[,] spiralArray = new int[rows, columns];
+FillSpiral(spiralArray);
+PrintArray(spiralArray);
+
+void FillSpiral(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    int countMax = rows > columns ? columns * 2 : rows * 2 - 1;
+    FillLine(arr, new int[] { -1, 0 }, new int[] { 1, 0 }, 0, 0, countMax);
+}
+
+void FillLine(int[,] arr, int[] pos, int[] dirVector, int offset, int count, int countMax)
+{
+    count++;
+    int startLimit, endLimit;
+    if (dirVector[0] != 0)
+    {
+        startLimit = offset;
+        endLimit = arr.GetLength(0) - offset;
+    }
+    else
+    {
+        startLimit = offset + 1;
+        endLimit = arr.GetLength(1) - offset - 1;
+    }
+
+    for (int i = startLimit; i < endLimit; i++)
+    {
+        arr[pos[0] + dirVector[0], pos[1] + dirVector[1]] = count;
+    }
+    dirVector[0] = -dirVector[1];
+    dirVector[1] = dirVector[0];
+    if (count % 4 == 0) offset++;
+    if (count <= countMax) FillLine(arr, pos, dirVector, offset, count, countMax);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j != 0) Console.Write(" ");
+            Console.Write($"{array[i, j]:D2}");
+        }
+        Console.WriteLine("|");
+    }
+}
