1787336f7c1c6f8a4c59b40e3289b1045960a7dd
Sergey_B <svbashkirov@gmail.com>
2022-06-08 18:09:28 +0300

    Task 8_1 8_2 started

---
+++ LESSON_8/TASK_8_1/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] matrix = CreateRandomMAtrix(3, 4, 10);
+Console.WriteLine(" Сгенерированная матрица:\n");
+PrintMatrix(matrix);
+int[,] swappedMatrix = CreateSwappedMatrix(matrix, 0, matrix.GetLength(0)-1);
+if (swappedMatrix.GetLength(0) < 2) { Console.WriteLine("\n Мало строк!"); Environment.Exit(0); }
+Console.WriteLine("\n Измененная матрица:\n");
+PrintMatrix(swappedMatrix);
+
+
+int[,] CreateRandomMAtrix(int rows, int columns, int limit)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(0, limit);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j != 0) Console.Write(" ");
+            Console.Write($"{matrix[i, j]:D2}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] CreateSwappedMatrix(int[,] arr, int row1, int row2)
+{
+    int rowNumber;
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    int[,] swappedArray = new int[rows, columns];
+for (int i = 0; i < rows; i++)
+    {
+        if(i == row1) rowNumber = row2;
+        else if (i == row2) rowNumber = row1;
+        else rowNumber = i;
+        for (int j = 0; j < columns; j++)
+        {
+            swappedArray[i,j] = arr[rowNumber,j];
+        }
+
+    }
+    return swappedArray;
+}
---
+++ LESSON_8/TASK_8_2/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] matrix = CreateRandomMAtrix(4, 4, 10);
+Console.WriteLine(" Сгенерированная матрица:\n");
+PrintMatrix(matrix);
+int[,] swappedMatrix = CreateSwappedMatrix(matrix);
+if (swappedMatrix.GetLength(0) != swappedMatrix.GetLength(1)) { Console.WriteLine("\n Ничего не получится!"); Environment.Exit(0); }
+Console.WriteLine("\n Измененная матрица:\n");
+PrintMatrix(swappedMatrix);
+
+
+int[,] CreateRandomMAtrix(int rows, int columns, int limit)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(0, limit);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j != 0) Console.Write(" ");
+            Console.Write($"{matrix[i, j]:D2}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] CreateSwappedMatrix(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    int[,] swappedArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            swappedArray[i, j] = arr[j, i];
+        }
+    }
+    return swappedArray;
+}
