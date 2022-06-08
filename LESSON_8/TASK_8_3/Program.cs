f384365eaf712e613f2deb3d1be46133937391c8
Sergey_B <svbashkirov@gmail.com>
2022-06-08 18:38:09 +0300

    Task 8_3 finished

--- LESSON_8/TASK_8_3/Program.cs
+++ LESSON_8/TASK_8_3/Program.cs
@@ -1,39 +1,60 @@
-﻿int[,,] matrix = CreateRandomMAtrix(3, 5, 6, 100);
+﻿int[,] matrix = CreateRandomMAtrix(4, 4, 10);
+Console.WriteLine(" Сгенерированная матрица:\n");
 PrintMatrix(matrix);
+int[,] sortedMatrix = CreateSortedMatrix(matrix);
+if (sortedMatrix.GetLength(0) != sortedMatrix.GetLength(1)) { Console.WriteLine("\n Ничего не получится!"); Environment.Exit(0); }
+Console.WriteLine("\n Измененная матрица:\n");
+PrintMatrix(sortedMatrix);
 
-int[,,] CreateRandomMAtrix(int dim1, int dim2, int dim3, int limit)
+
+int[,] CreateRandomMAtrix(int rows, int columns, int limit)
 {
-    List<int> originalValue = new List<int>();
-    int[,,] matrix = new int[dim1, dim2, dim3];
-    for (int i = 0; i < dim1; i++)
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < dim2; j++)
+        for (int j = 0; j < columns; j++)
         {
-            for (int k = 0; k < dim3; k++)
-            {
-                while (true)
-                {
-                    matrix[i, j, k] = new Random().Next(0, limit);
-                    if (!originalValue.Contains(matrix[i, j, k])) { originalValue.Add(matrix[i, j, k]); break; }
-                }
-            }
+            matrix[i, j] = new Random().Next(0, limit);
         }
     }
     return matrix;
 }
 
-void PrintMatrix(int[,,] matrix)
+void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        Console.Write("|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int k = 0; k < matrix.GetLength(2); k++)
-            {
-                Console.WriteLine($"Element [{i},{j},{k}] = {matrix[i, j, k]}");
-            }
-            Console.WriteLine();
+            if (j != 0) Console.Write(" ");
+            Console.Write($"{matrix[i, j]:D2}");
         }
-        Console.WriteLine();
+        Console.WriteLine("|");
     }
+}
+
+int[,] CreateSortedMatrix(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns - 1; j++)
+        {
+            int maxPos = j;
+            bool swap = false;
+            for (int k = j + 1; k < columns; k++)
+            {
+                if (arr[i,maxPos] < arr[i,k]) maxPos = k; swap = true;
+            }
+            if (swap)
+            {
+                int temp = arr[i,j];
+                arr[i,j] = arr[i,maxPos];
+                arr[i,maxPos] = temp;
+            }
+        }
+    }
+    return arr;
 }
